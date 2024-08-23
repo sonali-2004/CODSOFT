657ed68f007e43a5d97810a22da9fa079cbc7d94
sonali-2004 <sonalibehera879@gmail.com>
2024-08-23 18:49:14 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,74 +1,27 @@
-const screen = document.querySelector('.calculator-screen');
-const buttons = document.querySelectorAll('.btn');
-let currentInput = '';
-let previousInput = '';
-let operator = null;
+var box = document.getElementById("calc-display");
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const value = button.textContent;
-
-        if (value === 'C') {
-            clearScreen();
-        } else if (value === '=') {
-            calculate();
-        } else if (['+', '-', '*', '/'].includes(value)) {
-            chooseOperator(value);
-        } else {
-            appendNumber(value);
-        }
-    });
-});
-
-function appendNumber(number) {
-    currentInput += number;
-    updateScreen();
+function toScreen(x){
+  box.value+=x;
+  if(x==='C'){
+    box.value='';
+  }
 }
 
-function chooseOperator(op) {
-    if (currentInput === '') return;
-    if (previousInput !== '') calculate();
-    operator = op;
-    previousInput = currentInput;
-    currentInput = '';
+function answer(){
+  x=box.value;
+  x=eval(x);
+  box.value=x;
 }
 
-function calculate() {
-    let computation;
-    const prev = parseFloat(previousInput);
-    const curr = parseFloat(currentInput);
-    if (isNaN(prev) || isNaN(curr)) return;
-
-    switch (operator) {
-        case '+':
-            computation = prev + curr;
-            break;
-        case '-':
-            computation = prev - curr;
-            break;
-        case '*':
-            computation = prev * curr;
-            break;
-        case '/':
-            computation = prev / curr;
-            break;
-        default:
-            return;
-    }
-    
-    currentInput = computation;
-    operator = null;
-    previousInput = '';
-    updateScreen();
+function sqr(){
+  x=box.value;
+  x=eval(x*x);
+  box.value=x;
 }
 
-function clearScreen() {
-    currentInput = '';
-    previousInput = '';
-    operator = null;
-    updateScreen();
-}
-
-function updateScreen() {
-    screen.value = currentInput;
+function backSpace(){
+  var num = box.value;
+  var len = num.length-1;
+  var newNum = num.substring(0,len);
+  box.value = newNum;
 }
